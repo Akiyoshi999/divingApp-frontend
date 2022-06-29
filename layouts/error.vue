c37878018ff11ebe9c805cdde402e96c0dca8f4e
Akiyoshi <zundokokiyoshi999@gmail.com>
2022-06-30 08:46:38 +0900

    header add

--- layouts/error.vue
+++ layouts/error.vue
@@ -13,7 +13,7 @@
 <script>
 export default {
   name: 'EmptyLayout',
-  layout: 'empty',
+  layout: 'header',
   props: {
     error: {
       type: Object,
--- pages/index.vue
+++ pages/index.vue
@@ -83,5 +83,6 @@
 <script>
 export default {
   name: 'IndexPage',
+  layout: 'header',
 }
 </script>
